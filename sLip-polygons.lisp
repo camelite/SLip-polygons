966d582cd9869832372ef9a583d653fbf25626d1
camelite <conroydave@gmail.com>
2013-02-08 02:02:31 +0000

    Update sLip-polygons.lisp

--- sLip-polygons.lisp
+++ sLip-polygons.lisp
@@ -41,3 +41,69 @@
                               (rightf (/ 1 (* 2 n)))
                               (poly-recursive n (perpendicular-distance-to-pentagon-centre n r)))))
 
+(setf xy-global-multiplier 10)
+
+(defun xy-square (w)
+  (dotimes (i 4)
+    (forward (* w xy-global-multiplier))
+    (rightf (/ 4))))
+
+(defun xy-rect (h w)
+  (dotimes (i 2)
+    (forward (* h xy-global-multiplier))
+    (rightf (/ 4))
+    (forward (* w xy-global-multiplier))
+    (rightf (/ 4))))
+
+(defun xy-axis ()
+  (dotimes (i 2)
+    (forward 200)
+    (backward 400)
+    (forward 200)
+    (rightf (/ 4)))
+    (rightf (/ 2)))
+
+(defun shiftr (d)
+  (without-pen
+    (rightf (/ 4))
+    (forward (* d xy-global-multiplier))
+    (leftf (/ 4))))
+
+(defun shiftl (d)
+  (without-pen
+    (leftf (/ 4))
+    (forward (* d xy-global-multiplier))
+    (rightf (/ 4))))
+
+(defun xy-draw-f1 (a b)
+  (set-color "black")
+  (xy-square a)
+  (xy-rect b a)
+  (shiftr a)
+  (xy-square b)
+  (shiftl (/ a 2))
+  (set-color "grey")
+  (set-thickness (* a xy-global-multiplier))
+  (forward (* b xy-global-multiplier))
+  (backward (* b xy-global-multiplier))
+  (set-color "black")
+  (set-thickness 1)
+  (shiftl (/ a 2)))
+
+(setf iterator 0)
+
+(defun test-one-more (a b db)
+  (clear)
+  (xy-draw-f1 a b)
+  (setf iterator (+ b db))
+  (%console-log (+ (* a a)
+                   (- (* a b))
+                   (* b b))))
+
+(test-one-more 1 iterator (/ 1 20))
+
+(hide-turtle)
+
+(defun-js %console-log (string)
+  "console.log(string);")
+
